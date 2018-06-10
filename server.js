7636fd0186f56077fafe563afe30ceb077185af0
Alexander <alex.jce@gmail.com>
2018-06-10 18:40:39 +0300

    text / id - routes

--- server.js
+++ server.js
@@ -10,38 +10,58 @@ const jsonParser = bodyParser.json();
 const visionApiUrl = 'https://' + process.env.VISION_API_HOST + '/vision/v1.0';
 const visionApiKey = process.env.VISION_API_KEY;
 
-app.post('/ocr', jsonParser, function (req, res) {
+function validateRequest(req, res, next) {
     if (!req.body && req.body.url) return res.sendStatus(400);
     if (!req.body.secret) return res.sendStatus(401);
 
-    const requestOptions = {
-        url: visionApiUrl + '/ocr?language=en',
-        method: 'POST',
-        json: true,
-        headers: {
-            'Ocp-Apim-Subscription-Key': visionApiKey
-        },
-        body: {
-            url: req.body.url
-        }
-    };
+    next();
+}
 
-    request(requestOptions, (err, httpResponse, body) => {
-        if (!err && httpResponse.statusCode == 200) {
-            let text = [];
-            httpResponse.body.regions.forEach((region) => {
-                if (region.lines)
-                region.lines.forEach((line) => {
-                    if (line.words)
-                    line.words.forEach((word) => {
-                        text.push(word.text);
+function imageToText(url) {
+    return new Promise((resolve, reject) => {
+        const requestOptions = {
+            url: visionApiUrl + '/ocr?language=en',
+            method: 'POST',
+            json: true,
+            headers: { 'Ocp-Apim-Subscription-Key': visionApiKey },
+            body: { url: url }
+        };
+
+        return request(requestOptions, (err, httpResponse, body) => {
+            if (!err && httpResponse.statusCode == 200) {
+                let text = [];
+                httpResponse.body.regions.forEach((region) => {
+                    if (region.lines)
+                    region.lines.forEach((line) => {
+                        if (line.words)
+                        line.words.forEach((word) => {
+                            text.push(word.text);
+                        });
                     });
                 });
-            });
 
-            return res.send(text.join(" "));
-        } else return res.sendStatus(400);
+                resolve(text.join(" "));
+            } else reject(err);
+        });
     });
+
+}
+
+app.post('/image-to-text', jsonParser, validateRequest, (req, res) => {
+    return imageToText(req.body.url)
+        .then((text) => res.send(text))
+        .catch((err) => res.sendStatus(404));
 });
+
+app.post('/image-to-id', jsonParser, validateRequest, (req, res) => {
+    imageToText(req.body.url)
+        .then((text) => {
+            const matched = text.match(/\d{3}-?\d{7}-?\d{7}/g);
+            return (matched)
+                ? res.send(matched[0])
+                : res.sendStatus(404);
+        })
+        .catch((err) => res.sendStatus(400));
+})
 
 app.listen(process.env.PORT || 8080);
